e0ef069f718961ec8ba9ed616eb394c72e5d878f
mcgratheg <46490935+mcgratheg@users.noreply.github.com>
2019-08-26 21:33:06 +0100

    adding Node.js and Express
    adding Node.js and Express functionality to app
    created albums page to be built
    created index page with carousel function for latests images

---
+++ app.js
@@ -0,0 +1,35 @@
+// JavaScript source code
+var express = require("express"),
+    app = express(),
+    http = require("http").Server(app).listen(3306),
+    mysql = require("mysql");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "keyblade",
+    database: "photo_gallery"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+function getHomePage (req, res){
+    con.query("SELECT * FROM photo INNER JOIN album WHERE album.id = photo.album_id", function (err, result) {
+        if (err) throw err;
+        res.render(__dirname + "/index.ejs", {
+            photo: result
+        });
+    });
+};
+
+app.use("/css", express.static("./css"));
+app.use("/img", express.static("./img"));
+console.log("Server started at port 3306");
+app.get("/", getHomePage);
+app.get("/albums", function (req, res) {
+    res.render(__dirname + "/albums.ejs");
+});
