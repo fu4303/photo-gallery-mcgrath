394c6a1617b679a3784bbc53893e0eafcb6768dc
mcgratheg <46490935+mcgratheg@users.noreply.github.com>
2019-09-06 21:03:44 +0100

    adding validation to addphoto page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require("express"),
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "keyblade",
+    password: "",
     database: "photo_gallery"
 });
 
--- routes/album.js
+++ routes/album.js
@@ -1,32 +1,39 @@
 const fs = require("fs");
-let message = '';
+var message = '';
+exports.message = message;
 
 module.exports = {
     getAlbumsPage: (req, res) => {
+        message = '';
         let query = "SELECT album.id, album.name, DATE_FORMAT(album.date_created, '%d-%m-%Y') AS created_date, max(photo.path) AS photo_path, max(DATE_FORMAT(photo.date_uploaded, '%d-%m-%y')) AS last_upload FROM photo INNER JOIN album ON album.id=photo.album_id GROUP BY album.id ORDER BY album.date_created DESC;"
         con.query(query, function (err, result) {
             if (err) throw err;
             res.render("albums.ejs", {
+                message,
                 album: result
             });
         });
     },
 
     getAlbumPhotos: (req, res) => {
+        message = '';
         let albumId = req.params.id;
         let query = "SELECT * FROM photo INNER JOIN album ON photo.album_id=album.id WHERE album_id = " + albumId + " ORDER BY photo.id DESC";
         con.query(query, function (err, result) {
             if (err) throw err;
             res.render("album.ejs", {
+                message,
                 photos: result
             });
         });
     },
 
     getAddPhoto: (req, res) => {
-        con.query("SELECT * FROM album ORDER BY id DESC", function (err, result) {
+        let query = "SELECT * FROM album ORDER BY id DESC";
+        con.query(query, function (err, result) {
             if (err) throw err;
             res.render("addphoto.ejs", {
+                message,
                 albums: result
             });
         });
@@ -38,9 +45,10 @@ module.exports = {
         con.query(albumQuery, function (err, result) {
             if (err) throw err;
             if (result.length > 0) {
-                message = 'Album already exists';
+                message = "Album '"+albumName+"' already exists";
                 res.redirect("/addphoto");
             } else {
+                message = '';
                 let query = "INSERT INTO album (name) VALUES ('" + albumName + "')";
                 con.query(query, function (err, result) {
                     if (err) {
